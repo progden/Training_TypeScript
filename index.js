c782e9882b94c3b1e707369acb275d921cb4608a
Chia-Rong Chang <ccdrumer@gmail.com>
2017-07-14 16:33:33 +0800

    demo umd002

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 'use strict';
 
 // node index.js
-console.log("Hello World");
 
 var express = require('express');
 var app = express();
@@ -13,4 +12,25 @@ app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
+});
+
+var hightlightTool = require("./js/umd");
+
+app.get("/items", function(req, resp){
+    resp.send({
+            "code": "0000",
+            "items": [
+                {"name": "iPhone 7+", "price": 30000},
+                {"name": "iPhone 7", "price": 20000}
+        ]});
+});
+
+app.get("/testHighLight", function(req, resp){
+    var msg = "Hello Wrold!";
+    var keyword = "old";
+    resp.send({
+        msg: msg,
+        keyword: keyword,
+        rs: hightlightTool.highlight(msg, keyword)
+    });
 });
